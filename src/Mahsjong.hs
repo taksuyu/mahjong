95a9f3d0d648683986fd3f33eb5bbba2c3a96df9
TakSuyu <taksuyu@gmail.com>
2014-11-27 19:50:45 -0800

    split Tile functionality out of Mahsjong to it's own module and added Mahsjong.Tile to exposed modules.

--- src/Mahsjong.hs
+++ src/Mahsjong.hs
@@ -2,46 +2,7 @@ module Mahsjong where
 
 import           Data.Sequence
 
-data Tile = Character Int
-          | Circle Int
-          | Bamboo Int
-          | Wind Int
-          | Dragon Int
-          deriving (Eq, Ord, Show)
-
-position :: Tile -> Int
-position (Character a) = a
-position (Circle a) = a
-position (Bamboo a) = a
-position (Wind a) = a
-position (Dragon a) = a
-
-tileType :: Tile -> (Int -> Tile)
-tileType (Character _) = Character
-tileType (Circle _) = Circle
-tileType (Bamboo _) = Bamboo
-tileType (Wind _) = Wind
-tileType (Dragon _) = Dragon
-
---------------------------------------------------------------------------------
-tileMaxBound, nextDora :: Tile -> Tile
---------------------------------------------------------------------------------
-tileMaxBound (Wind _) = Wind 4
-tileMaxBound (Dragon _) = Dragon 3
-tileMaxBound a = tileType a 9
-
-nextDora a | a /= tileMaxBound a  = tileType a $ position a + 1
-           | otherwise = tileType a 1
-
---------------------------------------------------------------------------------
-isEnd, isSimple :: Tile -> Bool
---------------------------------------------------------------------------------
-isEnd (Wind _) = True
-isEnd (Dragon _) = True
-isEnd a | a == tileType a 1 || a == tileMaxBound a = True
-        | otherwise = False
-
-isSimple a = not $ isEnd a
+import           Mahsjong.Tile
 
 data MTile = MTile
              { tile    :: Tile
@@ -51,11 +12,11 @@ data MTile = MTile
 
 type Pile = Seq MTile
 
-data Player = East
-            | South
-            | West
-            | North
-            deriving (Show)
+data Seat = East
+          | South
+          | West
+          | North
+          deriving (Eq, Enum, Show)
 
 data Wall = Wall
             { wallTiles  :: Pile
@@ -64,8 +25,16 @@ data Wall = Wall
             , doraOffset :: Int
             } deriving (Show)
 
+data Player = Player
+              { seat     :: Seat
+              , points   :: Integer
+              , hand     :: Pile
+              , discards :: Pile
+              , inRiichi :: Bool
+              } deriving (Show)
+
 data Mahjong = Mahjong
-               { wall        :: Wall
-               , discardPile :: Seq (Player, Pile, Maybe Int)
-               , playerHand  :: Seq (Player, Pile)
+               { wall    :: Wall
+               , players :: [Player]
+               , round   :: (Seat, Int)
                } deriving (Show)
---
+++ src/Mahsjong/Tile.hs
@@ -0,0 +1,63 @@
+module Mahsjong.Tile where
+
+data Tile = Character Int
+          | Circle Int
+          | Bamboo Int
+          | Wind Int
+          | Dragon Int
+          deriving (Eq, Ord, Show)
+
+instance Enum Tile where
+  toEnum a | a >= 1  && a <= 9  = Character a
+           | a >= 10 && a <= 18 = Circle   (a - 9)
+           | a >= 19 && a <= 27 = Bamboo   (a - 18)
+           | a >= 28 && a <= 31 = Wind     (a - 27)
+           | a >= 32 && a <= 34 = Dragon   (a - 31)
+  toEnum _ = error "Tile.toEnum: Index out of bounds"
+
+  fromEnum (Character a) = a
+  fromEnum (Circle a)    = a + 9
+  fromEnum (Bamboo a)    = a + 18
+  fromEnum (Wind a)      = a + 27
+  fromEnum (Dragon a)    = a + 31
+
+position :: Tile -> Int
+position (Character a) = a
+position (Circle a)    = a
+position (Bamboo a)    = a
+position (Wind a)      = a
+position (Dragon a)    = a
+
+tileType :: Tile -> (Int -> Tile)
+tileType Character {} = Character
+tileType Circle {}    = Circle
+tileType Bamboo {}    = Bamboo
+tileType Wind {}      = Wind
+tileType Dragon {}    = Dragon
+
+--------------------------------------------------------------------------------
+tileMinBound, tileMaxBound, nextDora :: Tile -> Tile
+--------------------------------------------------------------------------------
+tileMinBound a = tileType a 1
+
+tileMaxBound Wind {} = Wind 4
+tileMaxBound Dragon {} = Dragon 3
+tileMaxBound a = tileType a 9
+
+nextDora a | a /= tileMaxBound a = succ a
+           | otherwise = tileMinBound a
+
+--------------------------------------------------------------------------------
+isTerminal, isEnd, isSimple :: Tile -> Bool
+--------------------------------------------------------------------------------
+isTerminal Wind {} = False
+isTerminal Dragon {} = False
+isTerminal a | a == tileMinBound a || a == tileMaxBound a = True
+             | otherwise = False
+
+isEnd Wind {} = True
+isEnd Dragon {} = True
+isEnd a | isTerminal a = True
+        | otherwise = False
+
+isSimple a = not $ isEnd a
