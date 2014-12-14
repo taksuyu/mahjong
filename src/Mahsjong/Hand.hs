d5869cc3d75eaf2732694a3482e0012ba439c471
TakSuyu <taksuyu@gmail.com>
2014-12-13 20:48:57 -0800

    added Some valid run conjecture functions

--- src/Mahsjong/Hand.hs
+++ src/Mahsjong/Hand.hs
@@ -10,20 +10,47 @@ data Meld = Pair Tile
           | Run (Tile, Tile, Tile)
           deriving (Eq, Show)
 
-tilesIn :: Tile -> [Tile] -> Int
-tilesIn a b = length $ elemIndices a b
+tileIn :: Tile -> [Tile] -> Int
+tileIn a b = length $ elemIndices a b
 
 filterTile :: Tile -> [Tile] -> [Tile]
 filterTile a b = filter (\n -> a /= n) b
 
 --------------------------------------------------------------------------------
-isDouble, isSet, isQuad :: Tile -> [Tile] -> Bool
+isDouble, isSet, isQuad, oTF, tTF, oTB, tTB :: Tile -> [Tile] -> Bool
 --------------------------------------------------------------------------------
-isDouble a b | tilesIn a b >= 2 = True
+isDouble a b | tileIn a b >= 2 = True
              | otherwise = False
 
-isSet a b | tilesIn a b >= 3 = True
+isSet a b | tileIn a b >= 3 = True
           | otherwise = False
 
-isQuad a b | tilesIn a b == 4 = True
+isQuad a b | tileIn a b == 4 = True
            | otherwise = False
+
+--------------------------------------------------------------------------------
+-- If oTF and tTF, oTF and oTB, or oTB and tTB exist in a given hand for
+-- an index that isn't a Wind or Dragon then you have a run in that
+-- hand.
+--------------------------------------------------------------------------------
+oTF a b | tileMaxBound a == a = False
+        | tileIn (succ a) b >= 1 = True
+        | otherwise = False
+
+tTF a b | tMB a == a || tMB sa == sa = False
+        | tileIn (succ sa) b >= 1 = True
+        | otherwise = False
+  where
+    tMB = tileMaxBound
+    sa = succ a
+
+oTB a b | tileMinBound a == a = False
+        | tileIn (pred a) b >= 1 = True
+        | otherwise = False
+
+tTB a b | tMB a == a || tMB pa == pa = False
+        | tileIn (pred pa) b >= 1 = True
+        | otherwise = False
+  where
+    tMB = tileMinBound
+    pa = pred a
