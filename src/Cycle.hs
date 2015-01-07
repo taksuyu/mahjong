a384094a45b447cdb323c4a366395f791fbea2df
raymoo <notanemail@fakeaddress.net>
2015-01-06 20:15:18 -0800

    add a bit

--- src/Cycle.hs
+++ src/Cycle.hs
@@ -3,7 +3,7 @@ module Cycle (Cycle(..)) where
 -- | A class for things you can increment, but which wrap around.
 -- If you have x, which is in a cycle, you should be able to get x back by
 -- repeatedly applying next (or prev). You should also be able to get any value
--- of x's type the same way.
+-- of x's type the same way. next . prev == id
 class Cycle a where
   next :: a -> a
   prev :: a -> a
