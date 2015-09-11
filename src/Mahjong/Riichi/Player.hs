d870734260668ae882d02d968942dec84744180d
taksuyu <tak@ispabuse.org>
2015-09-11 10:20:48 -0700

    added description

--- src/Mahjong/Riichi/Player.hs
+++ src/Mahjong/Riichi/Player.hs
@@ -1,5 +1,10 @@
 module Mahjong.Riichi.Player where
 
+-- | This module is for the @Player@ data structure which is used extensively
+-- throughout Riichi variants of mahjong. The logic behind many player related
+-- actions exists within @Mahjong.Riichi.Base@ as @Player@ generates some
+-- TemplateHaskell that we make use of to provide that functionality.
+
 import Control.Lens
 
 import Mahjong.Meld
